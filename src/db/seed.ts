ee12ba6fc6376a4606ab13c45b940e5a8e4fc38b
PetroPab <police.petrov@gmail.com>
2026-06-07 11:43:52 +0300

    фикс загрузки .env.local в drizzle-kit и seed-скрипте

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import { loadEnvConfig } from "@next/env";
+loadEnvConfig(process.cwd());
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
